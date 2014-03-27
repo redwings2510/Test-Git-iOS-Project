a7092588ced1e9f5a232441a9b8994133f96eddc
peterh <peterh@interreds-MacBook-Pro.local>
2014-03-27 16:06:37 +0100

    added a test class

---
+++ PHTest.h
@@ -0,0 +1,13 @@
+//
+//  PHTest.h
+//  Git Test
+//
+//  Created by peterh on 27.03.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PHTest : NSObject
+
+@end
---
+++ PHTest.m
@@ -0,0 +1,13 @@
+//
+//  PHTest.m
+//  Git Test
+//
+//  Created by peterh on 27.03.14.
+//
+//
+
+#import "PHTest.h"
+
+@implementation PHTest
+
+@end
